8831f2c541b4d81e7ef92dd3fae6af4dfa2f3fb0
GIGA360BR <renato.zerati@gmail.com>
2021-06-07 11:30:29 -0300

    Tour INVASE ver.1

---
+++ data.js
@@ -0,0 +1,429 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-clnica-invase-faixada",
+      "name": "Clínica INVASE Faixada",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "yaw": 0.24803604713386207,
+        "pitch": -0.36788979467942085,
+        "fov": 0.6024382110760899
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2475870756280507,
+          "pitch": -0.2529634592422134,
+          "rotation": 0,
+          "target": "1-entrada"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-entrada",
+      "name": "Entrada",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "yaw": 0.6766186961011798,
+        "pitch": -0.035669435844772224,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5840344856742998,
+          "pitch": 0.01928702562407736,
+          "rotation": 10.995574287564278,
+          "target": "2-recepo-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-recepo-1",
+      "name": "Recepção 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "yaw": -0.5079844979959578,
+        "pitch": 0.002972735025693396,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.769512117459918,
+          "pitch": 0.06165060602026884,
+          "rotation": 7.853981633974483,
+          "target": "3-recepo-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-recepo-2",
+      "name": "Recepção 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "yaw": -0.761389225342338,
+        "pitch": 0.059118942364055016,
+        "fov": 1.086993470035457
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6272868932313038,
+          "pitch": 0.1875120777261685,
+          "rotation": 1.5707963267948966,
+          "target": "4-recepo-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-recepo-3",
+      "name": "Recepção 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "yaw": -1.167710465011071,
+        "pitch": 0.01255886289890995,
+        "fov": 1.0534441537165515
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1375821626111176,
+          "pitch": 0.09443055664120692,
+          "rotation": 1.5707963267948966,
+          "target": "5-recepo-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-recepo-4",
+      "name": "Recepção 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "yaw": 0.046771574253275716,
+        "pitch": 0.03181435042329994,
+        "fov": 0.6523414039749316
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.039368379373989626,
+          "pitch": 0.15945700346524028,
+          "rotation": 5.497787143782138,
+          "target": "6-acesso--corredor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-acesso--corredor",
+      "name": "Acesso / Corredor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "yaw": -0.11340766408987513,
+        "pitch": 0.13790240043351432,
+        "fov": 0.6991602820655332
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.03545937911996866,
+          "pitch": 0.15288346657295548,
+          "rotation": 6.283185307179586,
+          "target": "7-consultrio-dr-alexandre"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-consultrio-dr-alexandre",
+      "name": "Consultório Dr Alexandre",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "yaw": -0.18887082052024695,
+        "pitch": -0.06739671002186753,
+        "fov": 0.7773172636930323
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.169315374477625,
+          "pitch": -0.10752698779810999,
+          "rotation": 1.5707963267948966,
+          "target": "8-consultrio-dra-mrcia"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-consultrio-dra-mrcia",
+      "name": "Consultório Dra Márcia",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "yaw": 0.44274469594583366,
+        "pitch": -0.10468638889141957,
+        "fov": 0.8394663260161223
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6648582972587604,
+          "pitch": -0.09803355008320125,
+          "rotation": 4.71238898038469,
+          "target": "9-sala-de-curativos"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-sala-de-curativos",
+      "name": "Sala de Curativos",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "yaw": 0.2671809714426949,
+        "pitch": 0.09542545823543058,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5376680642430447,
+          "pitch": 0.1118982180122714,
+          "rotation": 5.497787143782138,
+          "target": "10-sala-de-exames"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-sala-de-exames",
+      "name": "Sala de Exames",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "yaw": 0.22484559747362276,
+        "pitch": 0.25565521220903875,
+        "fov": 0.9956787956507072
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8399151553334328,
+          "pitch": 0.16572526881050464,
+          "rotation": 1.5707963267948966,
+          "target": "0-clnica-invase-faixada"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
